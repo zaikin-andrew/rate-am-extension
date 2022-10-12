b2d183f1694f854f18598b742fa44e867ea20990
Andrew Zaikin <zaikin.andry@gmail.com>
2022-10-12 19:05:53 +0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,15 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   public ardshinUsd: Observable<string>;
   public evocaUsd: Observable<string>;
+  public acbaUsd: Observable<string>;
   constructor(private http: HttpClient) {
     this.ardshinUsd = this.http.get(`https://rate.am/calculator/rates.ashx?cr1=USD&hcr=AMD&cr2=RUR&orgId=466fe84c-197f-4174-bc97-e1dc7960edc7&rtype=1&tp=0&l=lang3&r=${(new Date()).getTime()}`).pipe(
       map((rate: any) => (+rate.data.rates[0].buy/+rate.data.rates[1].sell).toFixed(2))
     );
     this.evocaUsd = this.http.get(`https://rate.am/calculator/rates.ashx?cr1=USD&hcr=AMD&cr2=RUR&orgId=0fffdcc4-8e36-49f3-9863-93ad02ce6541&rtype=1&tp=0&l=lang3&r=${(new Date()).getTime()}`).pipe(
+      map((rate: any) => (+rate.data.rates[0].buy/+rate.data.rates[1].sell).toFixed(2))
+    );
+    this.acbaUsd = this.http.get(`https://rate.am/calculator/rates.ashx?cr1=USD&hcr=AMD&cr2=RUR&orgId=f3ffb6cf-dbb6-4d43-b49c-f6d71350d7fb&rtype=1&tp=0&l=lang3&r=${(new Date()).getTime()}`).pipe(
       map((rate: any) => (+rate.data.rates[0].buy/+rate.data.rates[1].sell).toFixed(2))
     );
   }
